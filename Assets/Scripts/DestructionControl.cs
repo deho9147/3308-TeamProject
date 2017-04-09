5d26422d46c4bef1727fca067966a89491dad15b
Zunawe <gyroscope15@gmail.com>
2017-04-09 16:14:05 -0600

    Fix destruction/translate script to C#

---
+++ Assets/Scripts/DestructionControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionControl : MonoBehaviour {
+
+	public GameObject rubble;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			Instantiate(rubble, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
