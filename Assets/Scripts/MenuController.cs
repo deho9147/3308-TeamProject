2ec94ee4a3710a4e66285671fe1d0801dd596ca9
Zunawe <gyroscope15@gmail.com>
2017-04-09 17:14:19 -0600

    Added popup menu in main scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	private CanvasGroup menu;
+	
+	// Use this for initialization
+	void Start(){
+		menu = gameObject.GetComponent(typeof(CanvasGroup)) as CanvasGroup;
+		Hide();
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			if(menu.blocksRaycasts){
+				Hide();
+			}
+			else{
+				Show();
+			}
+		}
+	}
+
+	void Hide(){
+		menu.alpha = 0f;
+		menu.blocksRaycasts = false;
+		Camera.main.GetComponent<MouseLook>().active = true;
+	}
+
+	void Show(){
+		menu.alpha = 1f;
+		menu.blocksRaycasts = true;
+		Camera.main.GetComponent<MouseLook>().active = false;
+	}
+}
--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -7,6 +7,8 @@ public class MouseLook : MonoBehaviour {
     public float mouseSensitivity = 100.0f;
     public float clampAngle = 80.0f;
 
+    public bool active = true;
+
     private float rotY = 0.0f;
     private float rotX = 0.0f;
 
@@ -17,15 +19,17 @@ public class MouseLook : MonoBehaviour {
     }
 
     void Update(){
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = -Input.GetAxis("Mouse Y");
+        if(active){
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = -Input.GetAxis("Mouse Y");
 
-        rotY += mouseX * mouseSensitivity * Time.deltaTime;
-        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+            rotY += mouseX * mouseSensitivity * Time.deltaTime;
+            rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+            rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
 
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-        transform.rotation = localRotation;
+            Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+            transform.rotation = localRotation;
+        }
     }
 }
