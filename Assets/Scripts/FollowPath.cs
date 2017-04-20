84d9a550c2983c8561cfa12152e85ff236de4be4
Derek Holland <Derek Holland>
2017-04-19 21:04:52 -0600

    Created Script which makes object follow a path and started working on terrain"

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform[] path;
+    public float speed = 6.0f;
+    public float near = 1.0f;
+    public int currentpoint = 0;
+	void Start () {
+        transform.LookAt(path[currentpoint]);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(path[currentpoint].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position,path[currentpoint].position,Time.deltaTime*speed);
+        if (dist <= near)
+        {
+            currentpoint++;
+            if (currentpoint >= path.Length)
+            {
+                currentpoint = 0;
+            }
+            transform.LookAt(path[currentpoint]);
+        }
+	}
+
+    void OnDrawGizmos(){
+        if (path.Length > 0){
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] != null)
+                {
+                    Gizmos.DrawSphere(path[i].position, near);
+                }
+            }
+        }
+    }
+}
