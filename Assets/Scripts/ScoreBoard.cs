5337d0e927d5f82b0319ff84a3000e13eae81528
Jesse Tao <jeta7542@colorado.edu>
2017-04-29 16:34:25 -0600

    made things look more like a city and created a script to track score

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+    public int score = 0;
+    public int targetScore = 400;
+    public Text scoreText;
+    public GameObject goodJob;
+
+    void Awake()
+    {
+        scoreText.text = ("Score: " + score);
+    }
+
+    public void AddPoints(int pointScored)
+    {
+        score += pointScored;
+        scoreText.text = ("Score: " + score);
+    }
+
+    void allGood()
+    {
+        if(score >= targetScore)
+        {
+            goodJob.SetActive(true);
+        }
+    }
+}
