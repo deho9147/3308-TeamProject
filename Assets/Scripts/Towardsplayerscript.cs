a34db1228f917fe7824b5fad26d1a93309f0770b
Derek Holland <Derek Holland>
2017-05-04 15:48:27 -0600

    Added documentation to script

--- Assets/Scripts/Towardsplayerscript.cs
+++ Assets/Scripts/Towardsplayerscript.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+/** 
+ * make an object move towards the a specific location
+ * the goal location was the player
+ * this was unused in the final project
+ */
 public class Towardsplayerscript : MonoBehaviour {
     public Transform playerLoc;
 
